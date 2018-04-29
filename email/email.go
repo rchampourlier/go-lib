b854f3a401b5a250b1cac6ca8daee179834bd58a
Romain Champourlier <public@rchampourlier.com>
2018-04-29 19:25:15 +0200

    Improved email parsing
    - Handling new cases, such as email personal names containing a comma,
    or "undisclosed-recipients:;".
    - Encountering email parsing errors will not be fatal anymore. The
    parsing will be done anywhere it's possible and errors will be returned
    to indicated parsing issues.

--- email/email.go
+++ email/email.go
@@ -14,15 +14,17 @@ type Address struct {
 }
 
 // Parse parses an addresses string (e.g. "Me <me@me.com>")
-// and returns a slice of `Address` structs and an error if the
-// parsing failed.
-func Parse(s string) ([]*Address, error) {
-	rawAddresses := regexp.MustCompile(",[\\s]?").Split(s, -1)
+// and returns a slice of `Address` structs and a slice of the
+// parsing errors encountered.
+//
+// NB: even if errors are encountered, the function will return
+// as many addresses as it could parse.
+func Parse(s string) ([]*Address, []error) {
+	rawAddresses := splitAddressesString(s)
 	emailAddresses := []*Address{}
 	errors := make([]error, 0)
 
-	re := regexp.MustCompile("(?:\\A(.*)[\\s]<(.*)>\\z)|\\A(.*)\\z")
-
+	re := regexp.MustCompile("(?:\\A(.*)[\\s]<(.+@.+)>\\z)|\\A<?(.+@[^>]+)>?\\z")
 	for _, rawAddress := range rawAddresses {
 		matches := re.FindStringSubmatch(rawAddress)
 		if len(matches) == 4 {
@@ -36,14 +38,11 @@ func Parse(s string) ([]*Address, error) {
 			}
 			emailAddresses = append(emailAddresses, &ea)
 		} else {
-			errors = append(errors, fmt.Errorf("failed to parse `%s`", s))
+			errors = append(errors, fmt.Errorf("could not parse `%s`", rawAddress))
 		}
 	}
 
-	if len(errors) > 0 {
-		return emailAddresses, fmt.Errorf("%d email parsing errors", len(errors))
-	}
-	return emailAddresses, nil
+	return emailAddresses, errors
 }
 
 // DomainFromAddress extracts the domain from the email address.
@@ -52,7 +51,59 @@ func Parse(s string) ([]*Address, error) {
 //
 //   - `ea string`: email address (e.g. "me@domain.tld")
 //   - `n int`: number of domain path items to include
-func DomainFromAddress(ea string, n int) string {
-	items := regexp.MustCompile("[\\.]").Split(ea, -1)
-	return strings.Join(items[len(items)-n:], ".")
+func DomainFromAddress(ea string, n int) (string, error) {
+	matches := regexp.MustCompile("@(.*)").FindStringSubmatch(ea)
+	if len(matches) != 2 {
+		return "", fmt.Errorf("failed to extract domain from `%s`", ea)
+	}
+	if n < 1 {
+		return "", fmt.Errorf("`n` must be greater or equal to 1, got %d", n)
+	}
+
+	domainPart := matches[1]
+	items := regexp.MustCompile("[\\.]").Split(domainPart, -1)
+	firstItemIncluded := len(items) - n
+	if firstItemIncluded < 0 {
+		firstItemIncluded = 0
+	}
+	return strings.Join(items[firstItemIncluded:], "."), nil
+}
+
+func splitAddressesString(s string) []string {
+	// Basic split on commas
+	splits := regexp.MustCompile(",").Split(s, -1)
+	strings := make([]string, 0)
+	joinedString := ""
+
+	// Join strings that may have been split but should not
+	// (e.g. `"Personal, Name" <personal.name@tld.co`)
+	for _, split := range splits {
+		re := regexp.MustCompile("\\s*\"")
+		if re.MatchString(split) {
+			// The string contains a double-quote. It may be opening or
+			// closing a double-quoted substring.
+			split = re.ReplaceAllString(split, "") // removing the double-quote
+			if len(joinedString) == 0 {
+				// It's opening
+				joinedString = split
+			} else {
+				// It's closing
+				strings = append(strings, fmt.Sprintf("%s,%s", joinedString, split))
+				joinedString = ""
+			}
+		} else {
+			// The string doesn't contain a double-quote. It may however
+			// be inside a double-quoted substrings.
+			if len(joinedString) == 0 {
+				// Nope
+				split = regexp.MustCompile("(\\A\\s+)|(\\s+\\z)").ReplaceAllString(split, "")
+				strings = append(strings, split)
+			} else {
+				// Yep. Removing prefix/trailing spaces
+				joinedString = fmt.Sprintf("%s,%s", joinedString, split)
+			}
+		}
+	}
+
+	return strings
 }
--- email/email_test.go
+++ email/email_test.go
@@ -1,6 +1,7 @@
 package email_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/rchampourlier/golib/email"
@@ -9,56 +10,168 @@ import (
 type ParseCase struct {
 	Source    string
 	Addresses []*email.Address
+	Errors    []string
 }
 
 func Test_Email_Parse(t *testing.T) {
-	cases := []*ParseCase{
-		&ParseCase{
-			Source: "Me <me@domain.tld>",
-			Addresses: []*email.Address{
-				&email.Address{PersonalName: "Me", Address: "me@domain.tld"},
+	t.Run("normal_cases", func(t *testing.T) {
+		cases := []*ParseCase{
+			&ParseCase{
+				Source: "Me <me@domain.tld>",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "Me", Address: "me@domain.tld"},
+				},
+				Errors: []string{},
 			},
+			&ParseCase{
+				Source: " <me@domain.tld>",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "", Address: "me@domain.tld"},
+				},
+				Errors: []string{},
+			},
+			&ParseCase{
+				Source: "<me@domain.tld>",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "", Address: "me@domain.tld"},
+				},
+				Errors: []string{},
+			},
+			&ParseCase{
+				Source: "me@domain.tld",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "", Address: "me@domain.tld"},
+				},
+				Errors: []string{},
+			},
+			&ParseCase{
+				Source: "\"Me, of, course.\" <me@domain.tld>",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "Me, of, course.", Address: "me@domain.tld"},
+				},
+				Errors: []string{},
+			},
+			&ParseCase{
+				Source:    "undisclosed-recipients:;",
+				Addresses: []*email.Address{},
+				Errors:    []string{"could not parse `undisclosed-recipients:;`"},
+			},
+			&ParseCase{
+				Source:    "",
+				Addresses: []*email.Address{},
+				Errors:    []string{"could not parse ``"},
+			},
+			&ParseCase{
+				Source: "me@domain.tld, not-an-email",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "", Address: "me@domain.tld"},
+				},
+				Errors: []string{"could not parse `not-an-email`"},
+			},
+			&ParseCase{
+				Source: "Person1 <person1@dom.tld>, person2@dom.tld, Person3 Person3 <person3@dom.tld>, \"Person4, Inc\" <person4@dom.tld>",
+				Addresses: []*email.Address{
+					&email.Address{PersonalName: "Person1", Address: "person1@dom.tld"},
+					&email.Address{PersonalName: "", Address: "person2@dom.tld"},
+					&email.Address{PersonalName: "Person3 Person3", Address: "person3@dom.tld"},
+					&email.Address{PersonalName: "Person4, Inc", Address: "person4@dom.tld"},
+				},
+				Errors: []string{},
+			},
+		}
+		for _, c := range cases {
+			fmt.Printf("case `%s`\n", c.Source)
+			resultAddresses, errors := email.Parse(c.Source)
+
+			expectedAddresses := c.Addresses
+			if len(resultAddresses) != len(expectedAddresses) {
+				t.Errorf("expected to parse %d addresses, got %d", len(expectedAddresses), len(resultAddresses))
+			}
+
+			for i, ea := range expectedAddresses {
+				ra := resultAddresses[i]
+				if ra.PersonalName != ea.PersonalName {
+					t.Errorf("expected PersonalName to be `%s` for `%s`, got `%s`", ea.PersonalName, c.Source, ra.PersonalName)
+				}
+				if ra.Address != ea.Address {
+					t.Errorf("expected Address to be `%s` for `%s`, got `%s`", ea.Address, c.Source, ra.Address)
+				}
+			}
+
+			expectedErrors := c.Errors
+			if len(errors) != len(expectedErrors) {
+				t.Errorf("expected to have %d errors, got %d", len(expectedErrors), len(errors))
+			}
+
+			for i, ee := range expectedErrors {
+				e := errors[i]
+				if e.Error() != ee {
+					t.Errorf("expected error to be `%s` for `%s`, got `%s`", ee, c.Source, e.Error())
+				}
+			}
+		}
+	})
+}
+
+type DomainFromAddressCase struct {
+	Address string
+	Domain  string
+	Count   int
+}
+
+func Test_DomainFromAddress(t *testing.T) {
+	// Normal cases
+
+	cases := []DomainFromAddressCase{
+		DomainFromAddressCase{
+			Address: "me@domain.tld",
+			Domain:  "domain.tld",
+			Count:   2,
 		},
-		&ParseCase{
-			Source: " <me@domain.tld>",
-			Addresses: []*email.Address{
-				&email.Address{PersonalName: "", Address: "me@domain.tld"},
-			},
+		DomainFromAddressCase{
+			Address: "yes.me@domain.tld",
+			Domain:  "domain.tld",
+			Count:   2,
 		},
-		&ParseCase{
-			Source: "me@domain.tld",
-			Addresses: []*email.Address{
-				&email.Address{PersonalName: "", Address: "me@domain.tld"},
-			},
+		DomainFromAddressCase{
+			Address: "me@sub.domain.tld",
+			Domain:  "domain.tld",
+			Count:   2,
 		},
-		&ParseCase{
-			Source: "Person1 <person1@dom.tld>, person2@dom.tld, Person3 Person3 <person3@dom.tld>",
-			Addresses: []*email.Address{
-				&email.Address{PersonalName: "Person1", Address: "person1@dom.tld"},
-				&email.Address{PersonalName: "", Address: "person2@dom.tld"},
-				&email.Address{PersonalName: "Person3 Person3", Address: "person3@dom.tld"},
-			},
+		DomainFromAddressCase{
+			Address: "me@sub.domain.tld",
+			Domain:  "sub.domain.tld",
+			Count:   3,
+		},
+		DomainFromAddressCase{
+			Address: "me@domain.tld",
+			Domain:  "domain.tld",
+			Count:   10,
 		},
 	}
 	for _, c := range cases {
-		resultAddresses, err := email.Parse(c.Source)
+		r, err := email.DomainFromAddress(c.Address, c.Count)
 		if err != nil {
-			t.Fatal(err)
+			t.Errorf("unexpected error for email `%s`: %s", c.Address, err)
 		}
+		if r != c.Domain {
+			t.Errorf("expected domain `%s` for email `%s`, got `%s`", c.Domain, c.Address, r)
+		}
+	}
 
-		expectedAddresses := c.Addresses
-		if len(resultAddresses) != len(expectedAddresses) {
-			t.Errorf("expected to parse %d addresses, got %d", len(expectedAddresses), len(resultAddresses))
-		}
+	// Error cases
 
-		for i, ea := range expectedAddresses {
-			ra := resultAddresses[i]
-			if ra.PersonalName != ea.PersonalName {
-				t.Errorf("expected PersonalName to be `%s` for `%s`, got `%s`", ea.PersonalName, c.Source, ra.PersonalName)
-			}
-			if ra.Address != ea.Address {
-				t.Errorf("expected Address to be `%s` for `%s`, got `%s`", ea.Address, c.Source, ra.Address)
-			}
-		}
+	// Not an address
+	e := "not-an-email"
+	r, err := email.DomainFromAddress(e, 1)
+	if err == nil {
+		t.Errorf("expected an error for email `%s`, got result `%s`", e, r)
+	}
+
+	// n is not valid
+	n := -1
+	r, err = email.DomainFromAddress("me@domain.tld", n)
+	if err == nil {
+		t.Errorf("expected an error for n = %d, got result `%s`", n, r)
 	}
 }
